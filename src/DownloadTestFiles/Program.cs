feeab66c2815ea2696b91448cd93a19c111c3ef0
SIkebe <SIkebe@users.noreply.github.com>
2024-07-23 17:29:08 +0900

    Initial Commit

---
+++ src/DownloadTestFiles/Program.cs
@@ -0,0 +1,25 @@
+using DownloadTestFiles.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStaticFiles(new StaticFileOptions
+{
+	ServeUnknownFileTypes = true,
+	DefaultContentType = "application/octet-stream",
+});
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
